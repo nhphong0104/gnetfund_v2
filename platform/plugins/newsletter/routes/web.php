0a9374d780c2972960711259be376232320fde02
Phong Nguyen <nhphong0104@gmail.com>
2022-11-21 13:53:23 +0700

    up

--- platform/plugins/newsletter/routes/web.php
+++ platform/plugins/newsletter/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['namespace' => 'Botble\Newsletter\Http\Controllers', 'middleware' => ['web', 'core']], function () {
+Route::group(['namespace' => 'Botble\Newsletter\Http\Controllers', 'middleware' => ['web']], function () {
     Route::group(['prefix' => BaseHelper::getAdminPrefix(), 'middleware' => 'auth'], function () {
 
         Route::group(['prefix' => 'newsletters', 'as' => 'newsletter.'], function () {
